7542421295702ff887c5f478f6af82a0d8f7b341
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2021-02-12 17:14:38 +0530

    Create tests for auth routes

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -7,7 +7,6 @@ const router = express.Router();
 router.get(
   '/api/users/currentuser',
   currentUser,
-  requireAuth,
   (req: Request, res: Response) => {
     res.send({ currentUser: req.currentUser || null });
 });
