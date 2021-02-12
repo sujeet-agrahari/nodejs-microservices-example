cca7a9a52796e0d73ba79170c1c23fa991ec749d
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2021-02-12 11:12:51 +0530

    Create auth middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response} from 'express';
+import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-  res.send('Hi there!')
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  requireAuth,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express, { Request, Response} from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req: Request, res: Response) => {
-  res.send('Hi there!')
+  req.session = null;
+  res.send({});
 });
 
 export { router as signoutRouter };
